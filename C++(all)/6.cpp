68158bbfcb9da5a6f07d88c941f3e2ba4e1d9ece
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-24 12:41:38 +0600

    Add files via upload

--- C++(all)/6.cpp
+++ C++(all)/6.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 #include <conio.h>
+#include <iomanip>
+
 using namespace std;
 
-int main ()
+// showpoint
+// noshowpoint
+// setprecision
+
+int main()
 {
-   
-    char name[20];
-    float gpa;
+    float num1, num2, result;
+    cout << "Enter num1 : ";
+    cin >> num1;
 
-    // taking user input
-    cout << "Enter your name : " << endl;
-    cin >> name;
-    cout << "Enter your gpa : " << endl;
-    cin >> gpa;
+    cout << "Enter num2 : ";
+    cin >> num2;
 
-    //printing 
-    cout << "---------------------------" << endl;
-    cout << "Name : " << name << endl;
-    cout << "GPA : " << gpa << endl;
-    cout << "---------------------------" << endl;
-     getch();
+    cout << showpoint;
+
+    result = num1 + num2;
+    cout << setw(10) << "sum is : " << result << endl;
+
+    result = num1 - num2;
+    cout << setw(10) << "sub is : " << result << endl;
+
+    cout << noshowpoint;
+    result = num1 * num2;
+    cout << setw(10) << "Mul is : " << result << endl;
+
+    cout << fixed;
+    cout << setprecision(5);
+    result = num1 / num2;
+    cout << setw(10) << "div is : " << result <<endl;
+
+    // remainder is not possible between float number
+    // result = num1 % num2;
+    // cout << "Remainder is : " << result <<endl;
+
+
+    getch();
 }
