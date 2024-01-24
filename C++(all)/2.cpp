3d35d24db17302b24a7084712bc572d33036efe6
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-24 12:34:14 +0600

    Update 2.cpp

--- C++(all)/2.cpp
+++ C++(all)/2.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    cout << "Anisul Islam.\nM.Sc. in Software Development\n";
+    cout << "Maksud Alam.\nB.Sc. in CSE\n";
 
 
     cout << "I am from Bangladesh. My Phone number is \t 01444" << endl;
-    cout << "I am currently living in Finaland";
+    cout << "I am currently living in Bangladesh";
     getch();
 }
